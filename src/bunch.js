ec56a993476f78d1604580af7c0b9a405dc0eaf1
Xingguang Liu <75474957+lxg04@users.noreply.github.com>
2021-01-27 17:17:58 +0800

    Add files via upload

--- src/bunch.js
+++ src/bunch.js
@@ -7,6 +7,8 @@ var c0 = 299792458; //m.s-1, speed of light in vacuum
 var e = 1.6021766208e-19; //C, elementary charge
 var mu = 1.660539040e-27; //kg, atomic mass constant 
 
+// xingguang Liu(liuxg@ihep.ac.cn), Jan 27, 2021
+
 var muc2 = 931.4940954; //atomic mass constant energy equivalent in MeV
 
 
@@ -43,12 +45,41 @@ function single_bunch_charge(){
     
 }
 
+function Child_Langmuir(r, d, V){
+    // return Math.pow(V,1.5);
+    return 2.33e-6*Math.PI*Math.pow(r,2)*Math.pow(V,1.5)/Math.pow(d,2);
+}
+
+function cathode_pulse_extract(){
+    var r=document.getElementById("cathode_r").value/1000;
+    var d=document.getElementById("cathode_d").value/1000;
+    var V=document.getElementById("cathode_V").value*1000;
+    
+    document.getElementById("I_extract").value=Child_Langmuir(r,d,V);
+    
+    single_bunch_charge();
+    var Q0=document.getElementById("single_bunch_charge_gun").value;
+    document.getElementById("pulse_length_extract").value=Q0/Child_Langmuir(r,d,V);
+
+}
+
 function reset(){
     document.getElementById("circumference").value = 800;
     document.getElementById("f_main_ring").value = 500;
     document.getElementById("fill_rate_main_ring").value = 30;
     document.getElementById("average_current").value = 200;
-    document.getElementById("booster_transmission_rate").value = 100;
+    document.getElementById("booster_transmission_rate").value = 50;
     document.getElementById("linac_transmission_rate").value = 60;
+    document.getElementById("cathode_r").value = 8;
+    document.getElementById("cathode_d").value = 100;
+    document.getElementById("cathode_V").value = 150;
+
+    // cathode_pulse_extract();
 
 }
+
+function update(){
+    
+    cathode_pulse_extract();
+}
+
